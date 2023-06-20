3d307fb7ac465c10d5740359db6a0f9a05148828
~Gbenga018~ <72554595+Bode6192@users.noreply.github.com>
2023-06-20 11:20:40 +0100

    Delete Spring Boot Daily Buffer directory

--- Spring Boot Daily Buffer/spring-security-tutorial/src/main/java/com/dailycodebuffer/springsecuritytutorial/controller/RegistrationController.java
+++
@@ -1,2 +0,0 @@
-package com.dailycodebuffer.springsecuritytutorial.controller;public class RegistrationController {
-}
--- Spring Boot Daily Buffer/spring-security-tutorial/src/main/java/com/dailycodebuffer/springsecuritytutorial/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.dailycodebuffer.springsecuritytutorial.service;
-
-import com.dailycodebuffer.springsecuritytutorial.entity.User;
-import com.dailycodebuffer.springsecuritytutorial.model.UserModel;
-
-public interface UserService {
-
-    User registerUser(UserModel userModel);
-}
