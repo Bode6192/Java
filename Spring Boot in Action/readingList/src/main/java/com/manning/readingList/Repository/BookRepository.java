7061d1769e3e24892daa53323b299e3721fdcbf2
~Gbenga018~ <72554595+Bode6192@users.noreply.github.com>
2023-06-20 16:45:31 +0100

    Delete Spring Boot in Action directory

--- Spring Boot in Action/readingList/src/main/java/com/manning/readingList/Repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.manning.readingList.Repository;
-
-import com.manning.readingList.Entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
--- Spring Boot in Action/readingList/src/main/java/com/manning/readingList/Repository/ReaderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manning.readingList.Repository;
-
-import com.manning.readingList.Entity.Reader;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReaderRepository extends JpaRepository<Reader, String> {
-
-    Reader findByUsername(String username);
-}
