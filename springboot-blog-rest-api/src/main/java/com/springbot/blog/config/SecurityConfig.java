ee4fc157f58797db7156412481e861f976b58f6f
Gbenga <olabodegbenga018@gmail.com>
2023-06-27 14:33:18 +0100

    Converted the roles gotten from the user in the loadUserByUser method from the CustomUserDetailsService to a set of Granted Authorities. Removed the inMemoryUserDetailsManager for loading a User and added the Authentication Manager to the Security Config

--- springboot-blog-rest-api/src/main/java/com/springbot/blog/config/SecurityConfig.java
+++ springboot-blog-rest-api/src/main/java/com/springbot/blog/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.springbot.blog.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,15 +17,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Repository;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -38,21 +52,21 @@ public class SecurityConfig {
         return httpSecurity.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-
-        UserDetails gbenga = User.builder()
-                .username("gbenga")
-                .password(passwordEncoder().encode("gbenga018"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("abcd1234"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(gbenga, admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        UserDetails gbenga = User.builder()
+//                .username("gbenga")
+//                .password(passwordEncoder().encode("gbenga018"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("abcd1234"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(gbenga, admin);
+//    }
 }
--- springboot-blog-rest-api/src/main/java/com/springbot/blog/security/CustomUserDetailsService.java
+++ springboot-blog-rest-api/src/main/java/com/springbot/blog/security/CustomUserDetailsService.java
@@ -3,10 +3,15 @@ package com.springbot.blog.security;
 import com.springbot.blog.entity.User;
 import com.springbot.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -21,6 +26,14 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .orElseThrow(() ->
                         new UsernameNotFoundException("User not found with UserName or Email: " + userNameOrEmail));
 
-        return null;
+
+        Set<GrantedAuthority> authorities = user
+                .getRoles()
+                .stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(),
+                authorities);
     }
 }
